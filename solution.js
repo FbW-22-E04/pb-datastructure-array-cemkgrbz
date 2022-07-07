00ff3f67366e57b60a7cb3cffe6b07cf73fc95d4
Cem <cemkgrbz@gmail.com>
2022-07-07 13:35:29 +0200

    modified js file

---
+++ solution.js
@@ -0,0 +1,14 @@
+//1
+
+const myArray = ['cucumber', 'tomato', 'cheese', 'letuce', 'nutella']
+console.log(myArray)
+
+//2
+
+const thirdElement = myArray[2]
+console.log(thirdElement)
+
+//3
+
+myArray[3] = 'fourt element';
+console.log(myArray);
